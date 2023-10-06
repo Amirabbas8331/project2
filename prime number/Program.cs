f6e583010dbaa037994defdf0233e115f18c5a4a
Asus <Asus@DESKTOP-KU0AVPO>
2023-10-06 18:10:20 +0330

    Add project files.

---
+++ prime number/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please enter first number: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter second number: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            if (A < 2)
+            {
+
+                Console.WriteLine("please enter a number greater than 2 ");
+                Console.ReadLine();
+                return;
+
+            }
+            Console.WriteLine("The prime numbers between " + A + " and " + B + " are:");
+            int primenumber;
+            int i, j;
+            for (i = A; i <= B; i++)
+            {
+                primenumber = 0;
+
+                for (j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        primenumber = 1;
+                        break;
+                    }
+                }
+                if (primenumber == 0)
+                {
+
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ project1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pn, rev = 0; int rem;
+            Console.Write("please enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            pn = num;
+            while (num > 0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num = num / 10;
+            }
+            if (rev == pn)
+            {
+                Console.WriteLine($"{pn} is polindrome ");
+            }
+            else
+            {
+                Console.WriteLine($"{pn} is not polindrome ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
